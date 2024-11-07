53231e671b09af5f8bd2281f78e6cd23bae80791
ZenDev <ozenandreiii@gmail.com>
2024-11-07 18:58:48 +0800

    debug

--- backend/dbconfig.js
+++ backend/dbconfig.js
@@ -1,11 +1,21 @@
 import mysql from "mysql2";
-
+import dotenv from "dotenv";
+dotenv.config();
+// const db = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "password",
+//     database: "nlog"
+// })
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "nlog"
+ host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 })
+
+
 
 db.connect((err) => {
     if(err){
